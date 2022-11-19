43962852a7b24609b071cfce058a82c6f38fa9fc
azatprojazz <azatprojazz@gmail.com>
2022-11-19 22:54:44 +0300

    fix: изменить комменты, и названия обращений в DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,34 @@
-// Находим форму в DOM
+/** Находим форму в DOM, воспользуйтесь методом querySelector() */
 const popupElement = document.querySelector('.popup');
-
-// let formElement = // Воспользуйтесь методом querySelector()
 const formElement = popupElement.querySelector('.popup__form');
-
-// // Находим поля формы в DOM
+/** Находим поля формы в DOM */
 const nameElement = document.querySelector('.profile__name');
 const jobElement = document.querySelector('.profile__job');
-
-// let nameInput = // Воспользуйтесь инструментом .querySelector()
-const nameInput = formElement.querySelector('[name="name"]');
-
-// let jobInput = // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('[name="job"]');
-
+const nameInput = formElement.querySelector('#input-name');
+const jobInput = formElement.querySelector('#input-job');
 const popupEditBtnElement = document.querySelector('.profile__edit-btn');
 const popupCloseBtnElement = popupElement.querySelector('.popup__close-btn');
 
+/**
+ * Создаем класс, закрывающий Popup
+ */
 function closePopup() {
   popupElement.classList.remove('popup_opened');
 }
 
-// Получите значение полей jobInput и nameInput из свойства value
+/**
+ * Создаем класс, добавляющий Popup, и получаем значение полей jobInput и nameInput из свойства value
+ */
 function openPopup() {
   popupElement.classList.add('popup_opened');
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
 }
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-// Так мы можем определить свою логику отправки.
-// О том, как это делать, расскажем позже.
-function formSubmitHandler(evt) {
+/**
+ * Обработчик «отправки» формы, хотя пока она никуда отправляться не будет, так мы можем определить свою логику отправки.
+ */
+function submitForm(evt) {
   // Эта строчка отменяет стандартную отправку формы.
   evt.preventDefault();
   // Выберите элементы, куда должны быть вставлены значения полей
@@ -41,7 +38,7 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-// // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 popupEditBtnElement.addEventListener('click', openPopup);
 popupCloseBtnElement.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', submitForm);
