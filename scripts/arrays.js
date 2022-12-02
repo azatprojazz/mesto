e6bb38f7c412f7826bc1e55ef6a6ebba397d3229
azatprojazz <azatprojazz@gmail.com>
2022-12-02 16:42:25 +0300

    feat: добавить массив карточек

---
+++ scripts/arrays.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: 'Териберка',
+    link: '../images/place-teriberka.jpg',
+  },
+  {
+    name: 'Рускеала',
+    link: '../images/place-ruskeala.jpg',
+  },
+  {
+    name: 'Эльбрус',
+    link: '../images/place-elbrus.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: '../images/place-baykal.jpg',
+  },
+  {
+    name: 'Башкирия',
+    link: '../images/place-bashkiriya.jpg',
+  },
+  {
+    name: 'Алтай',
+    link: '../images/place-altay.jpg',
+  },
+];
+
+export default initialCards;
