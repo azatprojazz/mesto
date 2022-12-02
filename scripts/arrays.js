c22922ef964d73bf2a76de9357fdb613ac49a4eb
azatprojazz <azatprojazz@gmail.com>
2022-12-02 17:51:30 +0300

    fix: зафиксить пути картинок

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: 'Териберка',
-    link: '../images/place-teriberka.jpg',
+    link: './images/place-teriberka.jpg',
   },
   {
     name: 'Рускеала',
-    link: '../images/place-ruskeala.jpg',
+    link: './images/place-ruskeala.jpg',
   },
   {
     name: 'Эльбрус',
-    link: '../images/place-elbrus.jpg',
+    link: './images/place-elbrus.jpg',
   },
   {
     name: 'Байкал',
-    link: '../images/place-baykal.jpg',
+    link: './images/place-baykal.jpg',
   },
   {
     name: 'Башкирия',
-    link: '../images/place-bashkiriya.jpg',
+    link: './images/place-bashkiriya.jpg',
   },
   {
     name: 'Алтай',
-    link: '../images/place-altay.jpg',
+    link: './images/place-altay.jpg',
   },
 ];
 
