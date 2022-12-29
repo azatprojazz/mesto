221e24b23d00a10b248a139e79c49946bf15d17d
azatprojazz <azatprojazz@gmail.com>
2022-12-30 00:13:11 +0300

    fix: создать конфиг отдельно

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-btn',
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+};
+
+export default config;
