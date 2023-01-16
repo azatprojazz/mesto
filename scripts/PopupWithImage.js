67c079d1fc36776f882c0753eea87e3857dbdb02
azatprojazz <azatprojazz@gmail.com>
2023-01-16 15:33:11 +0300

    feat: реализовать класс popupWithImage

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from './Popup.js';
+
+export default class PopupWithImage extends Popup {
+  constructor(selectorPopup) {
+    super(selectorPopup);
+  }
+
+  open(name, link) {
+    this._image = this._popup.querySelector('.popup__image');
+    this._caption = this._popup.querySelector('.popup__caption');
+    this._image.alt = name;
+    this._image.src = link;
+    this._caption.textContent = name;
+    super.open();
+  }
+}
