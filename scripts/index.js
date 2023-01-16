d72dfbca40a450acf8f179037de0c0e474c7bd1b
azatprojazz <azatprojazz@gmail.com>
2023-01-16 15:36:56 +0300

    fix: применить реализованные классы в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,32 @@
 import initialCards from './initialCardsData.js';
-import { Card } from './Card.js';
+import Card from './Card.js';
 import config from './config.js';
 import FormValidator from './FormValidator.js';
-
-// Находим все попапы
-const popups = document.querySelectorAll('.popup');
-const popupProfile = document.querySelector('.popup_type_profile');
-const popupCards = document.querySelector('.popup_type_cards');
-const popupViewCard = document.querySelector('.popup_type_view-card');
-
-// Обращаемся к содержимому template
-const cardsContainer = document.querySelector('.cards__container');
-
-// Находим формы в DOM
-const formEditProfile = popupProfile.querySelector('.popup__form');
-const formAddCard = popupCards.querySelector('.popup__form');
+import Section from './Section.js';
+import PopupWithImage from './PopupWithImage.js';
+import PopupWithForm from './PopupWithForm.js';
+import UserInfo from './UserInfo.js';
 
 // Вытаскиваем элементы из DOM для добавления карточек
 const popupCardsOpenBtn = document.querySelector('.profile__add-btn');
-const popupImageElement = popupViewCard.querySelector('.popup__image');
-const popupCaptionElement = popupViewCard.querySelector('.popup__caption');
 
-// Находим поля формы в DOM
-const nameElement = document.querySelector('.profile__name');
-const jobElement = document.querySelector('.profile__job');
-const nameCardInput = formAddCard.querySelector(
-  '.popup__input_content_card-name'
-);
-const linkCardInput = formAddCard.querySelector(
-  '.popup__input_content_card-link'
-);
-const nameInput = formEditProfile.querySelector('.popup__input_content_name');
-const jobInput = formEditProfile.querySelector('.popup__input_content_job');
+const nameInput = document.querySelector('.popup__input_content_name');
+const jobInput = document.querySelector('.popup__input_content_job');
 
 // Находим кнопки
 const popupEditBtnElement = document.querySelector('.profile__edit-btn');
 
-// Закрытие попап по кнопке Escape
-const handleKeyUp = (evt) => {
-  if (evt.key === 'Escape') {
-    const openedModal = document.querySelector('.popup_opened'); //найди мне открытую модалку
+const popupProfile = new PopupWithForm(
+  '.popup_type_profile',
+  submitProfileForm
+);
+popupProfile.setEventListeners();
 
-    closePopup(openedModal); //закрой мне эту модалку
-  }
-};
+const popupCards = new PopupWithForm('.popup_type_cards', submitAddCard);
+popupCards.setEventListeners();
+
+const popupViewCard = new PopupWithImage('.popup_type_view-card');
+popupViewCard.setEventListeners();
 
 const createCard = (item) => {
   const card = new Card(item, '.card-template', handleClickCard);
@@ -52,88 +35,68 @@ const createCard = (item) => {
   return cardElement;
 };
 
-/**
- * Пробегаемся по списку массива карточек, создаем карточки
- */
-initialCards.forEach((item) => {
-  const cardElement = createCard(item);
-  cardsContainer.prepend(cardElement);
-});
+const cardSection = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const cardElement = createCard(item);
+      cardSection.addItem(cardElement);
+    },
+  },
+  '.cards__container'
+);
+cardSection.renderItems();
 
-/**
- * Создаем класс, закрывающий Popup
- */
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', handleKeyUp);
-}
+const userInfo = new UserInfo({
+  nameSelector: '.profile__name',
+  jobSelector: '.profile__job',
+});
 
 /**
  * Открываем попапы
  */
 function handleClickCard(name, link) {
-  popupImageElement.alt = name;
-  popupImageElement.src = link;
-  popupCaptionElement.textContent = name;
-  openPopup(popupViewCard);
-}
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', handleKeyUp);
+  popupViewCard.open(name, link);
 }
 
 /**
  * Создаем класс, добавляющий Popup, и получаем значение полей jobInput и nameInput из свойства value
  */
 function openPopupProfile() {
-  openPopup(popupProfile);
-  nameInput.value = nameElement.textContent;
-  jobInput.value = jobElement.textContent;
+  const userData = userInfo.getUserInfo();
+  nameInput.value = userData.name;
+  jobInput.value = userData.job;
+  popupProfile.open();
 }
 
 function openPopupCard() {
-  openPopup(popupCards);
+  popupCards.open();
 }
 
 /**
  * Обработчик «отправки» формы, хотя пока она никуда отправляться не будет, так мы можем определить свою логику отправки.
  */
-function submitProfileForm(evt) {
+function submitProfileForm(evt, inputValues) {
   // Эта строчка отменяет стандартную отправку формы.
   evt.preventDefault();
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // Вставьте новые значения с помощью textContent
-  nameElement.textContent = nameInput.value;
-  jobElement.textContent = jobInput.value;
-  closePopup(popupProfile);
+  userInfo.setUserInfo(inputValues);
+  popupProfile.close();
 }
 
-formAddCard.addEventListener('submit', function (evt) {
+function submitAddCard(evt, inputValues) {
   evt.preventDefault();
   const cardElement = createCard({
-    name: nameCardInput.value,
-    link: linkCardInput.value,
+    name: inputValues.name,
+    link: inputValues.link,
   });
-  cardsContainer.prepend(cardElement);
-  closePopup(popupCards);
-  evt.target.reset();
-});
+  // console.log(popupCards._getInputValues())
+  cardSection.addItem(cardElement);
+  popupCards.close();
+}
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 popupEditBtnElement.addEventListener('click', openPopupProfile);
 popupCardsOpenBtn.addEventListener('click', openPopupCard);
-popups.forEach((popup) => {
-  popup.addEventListener('mousedown', (evt) => {
-    if (
-      evt.target.classList.contains('popup') ||
-      evt.target.classList.contains('popup__close-btn')
-    ) {
-      closePopup(popup);
-    }
-  });
-});
-formEditProfile.addEventListener('submit', submitProfileForm);
 
 const forms = document.querySelectorAll(config.formSelector); //находим все формы на странице
 forms.forEach((form) => {
