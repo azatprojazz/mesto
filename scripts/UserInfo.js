33d6079074925b68b286dfce38219190e1ad7729
azatprojazz <azatprojazz@gmail.com>
2023-01-16 15:31:29 +0300

    feat: реализовать класс userinfo

---
+++ scripts/UserInfo.js
@@ -0,0 +1,18 @@
+export default class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._jobElement = document.querySelector(jobSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      job: this._jobElement.textContent,
+    };
+  }
+
+  setUserInfo({ name, job }) {
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
+  }
+}
