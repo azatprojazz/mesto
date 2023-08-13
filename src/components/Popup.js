4b323df9bed8d3e2201bfd10d6baeff9ef2d0770
azatprojazz <azatprojazz@gmail.com>
2023-08-13 18:16:12 +0300

    fix: улучшение читаемости кода и добавление комментариев к методам класса Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,32 +1,36 @@
 export default class Popup {
+  // Конструктор класса Popup принимает следующий аргумент:
+  // selectorPopup: селектор всплывающего окна
   constructor(selectorPopup) {
     this._popup = document.querySelector(selectorPopup);
 
+    // Привязываем контекст, чтобы можно было передавать _handleEscClose как обработчик событий
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
+  // Открывает всплывающее окно и добавляет обработчик закрытия по нажатию на клавишу Escape
   open() {
     this._popup.classList.add('popup_opened');
     document.addEventListener('keydown', this._handleEscClose);
   }
 
+  // Закрывает всплывающее окно и удаляет обработчик закрытия по нажатию на клавишу Escape
   close() {
     this._popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
   }
 
+  // Обрабатывает закрытие всплывающего окна по нажатию на клавишу Escape
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
-      this.close(); //закрой мне эту модалку
+      this.close();
     }
   }
 
+  // Устанавливает обработчики событий для закрытия всплывающего окна по клику на оверлей или кнопку закрытия
   setEventListeners() {
     this._popup.addEventListener('mousedown', (evt) => {
-      if (
-        evt.target === this._popup ||
-        evt.target.classList.contains('popup__close-btn')
-      ) {
+      if (evt.target === this._popup || evt.target.classList.contains('popup__close-btn')) {
         this.close();
       }
     });
