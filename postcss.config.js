5ebd71db6cbe70710ef4121445770736818f7b17
azatprojazz <azatprojazz@gmail.com>
2023-08-13 17:28:52 +0300

    fix: исправление табуляции в файлах

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,6 @@ module.exports = {
     // cssnano при подключении нужно передать объект опций
     // { preset: default } говорит о том, что нужно использовать
     // стандартные настройки минификации
-    cssnano({ preset: 'default' })
-  ]
+    cssnano({ preset: 'default' }),
+  ],
 };
