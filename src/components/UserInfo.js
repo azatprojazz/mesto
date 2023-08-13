3026c29f9ba6bf66de73018144c98e61a34bc6ca
azatprojazz <azatprojazz@gmail.com>
2023-08-13 18:19:14 +0300

    fix: добавлены комментарии к методам и конструктору класса UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,10 +1,13 @@
 export default class UserInfo {
+  // Конструктор класса UserInfo принимает следующие аргументы:
+  // { nameSelector, jobSelector, avatarSelector }: объект с селекторами элементов имени, описания и аватара пользователя
   constructor({ nameSelector, jobSelector, avatarSelector }) {
     this._nameElement = document.querySelector(nameSelector);
     this._jobElement = document.querySelector(jobSelector);
     this._avatarElement = document.querySelector(avatarSelector);
   }
 
+  // Возвращает информацию о пользователе в виде объекта с полями name и about
   getUserInfo() {
     return {
       name: this._nameElement.textContent,
@@ -12,6 +15,7 @@ export default class UserInfo {
     };
   }
 
+  // Устанавливает информацию о пользователе на странице, обновляя текстовое содержимое и атрибуты элементов
   setUserInfo({ name, about, avatar, _id }) {
     this._nameElement.textContent = name;
     this._jobElement.textContent = about;
@@ -19,6 +23,7 @@ export default class UserInfo {
     this._id = _id;
   }
 
+  // Возвращает идентификатор пользователя
   getId() {
     return this._id;
   }
