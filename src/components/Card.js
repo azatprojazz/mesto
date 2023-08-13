bfd99cb3ee4af9a414724d6e3516aa4b7bb9c041
azatprojazz <azatprojazz@gmail.com>
2023-08-13 17:45:32 +0300

    fix: добавлены комментарии для методов и улучшена читаемость кода класса Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 export default class Card {
-  constructor(
-    data,
-    templateSelector,
-    userId,
-    handleClickCard,
-    handleDeleteCard,
-    handleLikeClick
-  ) {
+  // Конструктор класса Card принимает следующие аргументы:
+  // data: объект с данными карточки (имя, ссылка, лайки, владелец и ID)
+  // templateSelector: селектор шаблона карточки
+  // userId: ID текущего пользователя
+  // handleClickCard: функция-обработчик клика по изображению карточки
+  // handleDeleteCard: функция-обработчик удаления карточки
+  // handleLikeClick: функция-обработчик клика по лайку карточки
+  constructor(data, templateSelector, userId, handleClickCard, handleDeleteCard, handleLikeClick) {
     this._name = data.name;
     this._link = data.link;
     this._likes = data.likes;
@@ -20,6 +20,7 @@ export default class Card {
     this._handleLikeClick = handleLikeClick;
   }
 
+  // Возвращает клонированный шаблон карточки из HTML
   _getTemplate() {
     const cardElement = document
       .querySelector(this._templateSelector)
@@ -29,26 +30,32 @@ export default class Card {
     return cardElement;
   }
 
+  // Генерирует карточку с данными и добавляет обработчики событий
   generateCard() {
-    this._element = this._getTemplate(); // возвращает мою li из шаблона template
+    this._element = this._getTemplate();
     this._cardImage = this._element.querySelector('.card__image');
     this._likeCountElement = this._element.querySelector('.card__like-count');
     this._elementRemoveBtn = this._element.querySelector('.card__delete-btn');
 
+    // Удаляет кнопку удаления карточки, если текущий пользователь не является владельцем карточки
     if (this._ownerId !== this._userId) {
       this._elementRemoveBtn.remove();
     }
 
+    // Устанавливает текст и изображение карточки
     this._element.querySelector('.card__title').textContent = this._name;
     this._cardImage.alt = this._name;
     this._cardImage.src = this._link;
     this._likeCountElement.textContent = this._likes.length;
+
+    // Добавляет обработчики событий
     this._setEventListeners();
     this._setLikeColor();
 
     return this._element;
   }
 
+  // Устанавливает цвет кнопки лайка в зависимости от состояния лайка
   _setLikeColor() {
     if (this._isLike) {
       this._likeButton.classList.add('card__like_active');
@@ -57,15 +64,18 @@ export default class Card {
     }
   }
 
+  // Изменяет количество лайков карточки
   changeLikesCount(numberLike) {
     this._likeCountElement.textContent = numberLike;
   }
 
+  // Изменяет состояние лайка (добавляет или удаляет лайк)
   changeLike() {
     this._isLike = !this._isLike;
     this._setLikeColor();
   }
 
+  // Устанавливает обработчики событий для кнопок лайка, удаления карточки и клика по изображению карточки
   _setEventListeners() {
     const card = this;
     this._likeButton = this._element.querySelector('.card__like');
@@ -83,10 +93,7 @@ export default class Card {
     });
   }
 
-  _handleLikeToggle() {
-    this._likeButton.classList.toggle('card__like_active');
-  }
-
+  // Удаляет карточку из DOM
   handleRemoveClick() {
     this._element.remove();
     this._element = null;
