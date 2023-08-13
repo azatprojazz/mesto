a155c4c6378ac1c13ddd7f510d25f10ee3681112
azatprojazz <azatprojazz@gmail.com>
2023-08-13 18:17:59 +0300

    fix: добавление комментариев к методам и конструктору класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,12 +1,15 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  // Конструктор класса PopupWithImage принимает следующий аргумент:
+  // selectorPopup: селектор всплывающего окна
   constructor(selectorPopup) {
     super(selectorPopup);
     this._image = this._popup.querySelector('.popup__image');
     this._caption = this._popup.querySelector('.popup__caption');
   }
 
+  // Открывает всплывающее окно с изображением и устанавливает атрибуты изображения и текст подписи
   open(name, link) {
     this._image.alt = name;
     this._image.src = link;
