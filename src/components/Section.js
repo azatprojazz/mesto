e236ef3f57608bb2ed984288b5e902355e912a78
azatprojazz <azatprojazz@gmail.com>
2023-08-13 18:18:36 +0300

    fix: добавлены комментарии для методов и параметров класса `Section`

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,19 @@
 export default class Section {
+  // Конструктор класса Section принимает следующие аргументы:
+  // { renderer }: объект с функцией-рендерером для отображения элементов
+  // selector: селектор контейнера, в котором будут отображаться элементы
   constructor({ renderer }, selector) {
     this._renderer = renderer;
     this._container = document.querySelector(selector);
   }
 
+  // Добавляет элемент в начало контейнера
   addItem(element) {
     this._container.prepend(element);
   }
 
+  // Отображает элементы с помощью функции-рендерера в обратном порядке (сначала последний элемент)
   renderItems(items) {
-    items.reverse().forEach((item) => this._renderer(item)); // большое спасибо за подсказку .reverse()
+    items.reverse().forEach((item) => this._renderer(item));
   }
 }
